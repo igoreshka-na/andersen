129a037a84e4e0a1498ed9a2526b6354ab4e1b7b
Igor Novikov <igos_96@mail.ru>
2021-10-20 19:25:59 +0300

    private methods and add(two), get + little bit test

--- MyArrayList/src/main/java/MyArrayList.java
+++ MyArrayList/src/main/java/MyArrayList.java
@@ -1,30 +1,89 @@
+import java.util.Arrays;
 import java.util.Collection;
 
 public class MyArrayList<T> {
     private static final int DEFAULT_CAPACITY = 10;
     private int size;
+    private int lastObject = 0;
     private T[] elements;
 
     public MyArrayList(int initialSize) {
-        if (initialSize < 0) {
-            this.size = initialSize;
-            this.elements = (T[]) new Object[initialSize];
+        if (initialSize > 0) {
+            size = initialSize;
+            elements = (T[]) new Object[size];
         } else if (initialSize == 0) {
-            this.elements = (T[]) new Object[] {};
+            elements = (T[]) new Object[] {};
+            size = 0;
         } else throw new IllegalArgumentException("Illegal size: " + initialSize);
     }
 
     public MyArrayList() {
-        this.size = DEFAULT_CAPACITY;
-        this.elements = (T[]) new Object[size];
+        size = DEFAULT_CAPACITY;
+        elements = (T[]) new Object[size];
     }
 
-    public MyArrayList(Collection<? extends T> collection) {
+    public MyArrayList(Collection<?> collection) {
         elements = (T[]) collection.toArray();
+        size = elements.length;
+        lastObject = elements.length;
+    }
+
+    private void capacityExpansion() {
+        int newCapacity = (size * 3) / 2 + 1;
+        elements = Arrays.copyOf(elements, newCapacity);
+        size = newCapacity;
+    }
+
+    private boolean ensureCapacity() {
+        return lastObject != size;
+    }
+
+    private void rangeCheckElement(int index) {
+        if (0 > index || lastObject < index)
+            throw new IndexOutOfBoundsException("Index: " + index + ", Elements last index: " + lastObject);
+    }
+
+    public int size() { return size; }
+
+    public void add(T element) {
+        if (ensureCapacity()) {
+            elements[lastObject] = element;
+            lastObject++;
+        } else {
+            capacityExpansion();
+            elements[lastObject] = element;
+            lastObject++;
+        }
+    }
+
+    public void add(int index, T object) {
+        if (!ensureCapacity()) {
+            capacityExpansion();
+            System.arraycopy(elements, index, elements, index + 1, size - 1);
+            elements[index] = object;
+            lastObject++;
+        } else {
+            System.arraycopy(elements, index, elements, index + 1, size - 1);
+            elements[index] = object;
+            lastObject++;
+        }
+    }
+
+    public T get(int index) {
+        rangeCheckElement(index);
+        return elements[index];
     }
 }
 class Main {
     public static void main(String[] args) {
         MyArrayList<String> list = new MyArrayList<>(10);
+        for (int i = 0; i < 14; i++) {
+            list.add(String.valueOf(i));
+            System.out.println(i + " - " + list.size());
+        }
+        list.add(0, "full");
+        System.out.println(list.get(0));
+        System.out.println();
+        list.get(1);
     }
 }
--- MyArrayList/src/test/java/MyArrayListTest.java
+++ MyArrayList/src/test/java/MyArrayListTest.java
@@ -1,2 +1,18 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class MyArrayListTest {
+    private MyArrayList<Object> list = new MyArrayList<>(10);
+
+    @Test
+    public void checkSizeList() {
+        assertEquals(10, list.size());
+    }
+
+    @Test
+    public void checkTypeObjects() {
+        list.add(new Object());
+        assertEquals(Object.class, list.get(0).getClass());
+    }
 }
