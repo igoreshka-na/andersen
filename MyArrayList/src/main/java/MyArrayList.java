64528757d930e4847f84dfac797111b0cc4b5545
Igor Novikov <igos_96@mail.ru>
2021-10-20 15:50:04 +0300

    created constructors

--- MyArrayList/src/main/java/MyArrayList.java
+++ MyArrayList/src/main/java/MyArrayList.java
@@ -1,19 +1,30 @@
+import java.util.Collection;
+
 public class MyArrayList<T> {
-    private static final int DEFAULT_CAPACITY = 16;
+    private static final int DEFAULT_CAPACITY = 10;
     private int size;
-    private Object[] elements;
+    private T[] elements;
 
     public MyArrayList(int initialSize) {
         if (initialSize < 0) {
             this.size = initialSize;
-            this.elements = new Object[initialSize];
+            this.elements = (T[]) new Object[initialSize];
         } else if (initialSize == 0) {
-            this.elements = new Object[] {};
+            this.elements = (T[]) new Object[] {};
         } else throw new IllegalArgumentException("Illegal size: " + initialSize);
     }
 
     public MyArrayList() {
         this.size = DEFAULT_CAPACITY;
-        this.elements = new Object[DEFAULT_CAPACITY];
+        this.elements = (T[]) new Object[size];
+    }
+
+    public MyArrayList(Collection<? extends T> collection) {
+        elements = (T[]) collection.toArray();
+    }
+}
+class Main {
+    public static void main(String[] args) {
+        MyArrayList<String> list = new MyArrayList<>(10);
     }
 }
