0f5d31efad7cb728607089a4317e064ded3b6218
Igor Novikov <igos_96@mail.ru>
2021-11-08 17:12:14 +0300

    added pom.xml for client + chenged port service

--- SOAP-service-command/src/main/java/server/SoapServer.java
+++ SOAP-service-command/src/main/java/server/SoapServer.java
@@ -5,7 +5,7 @@ import service.SOAPclassComand;
 import javax.xml.ws.Endpoint;
 
 public class SoapServer {
-    private static final String url = "http://localhost:8080/soap";
+    private static final String url = "http://localhost:8081/teamService";
     public static void main(String[] args) {
         Endpoint.publish(url, new SOAPclassComand());
     }
